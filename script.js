758ff9e01d23d757ac117cb5cb6745e85a50badd
dinyardin <dinyardin@gmail.com>
2021-10-26 01:05:00 -0400

    Added a functionality to go to previous comics in iframe

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-document.getElementById("button1").addEventListener("click", getNextComic);
-
-//https://rocky-sands-68974.herokuapp.com/
-
-function getNextComic() {
-  async function getData() {
-    const response = await fetch("https://xkcd.com/info.0.json");
-
-    const data = await response.json();
-
-    //console.log(data);
-    return data;
-  }
-
-  getData()
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
-
-//https://xkcd.com/info.0.json
+let currentNumber = null;
 
 function getCurrentComicNumber() {
   document.getElementById("next").disabled = true;
@@ -34,9 +11,22 @@ function getCurrentComicNumber() {
   getCurrentNumber()
     .then((res) => {
       console.log(res.num);
+      currentNumber = res.num;
       document.getElementById("iframe").setAttribute("src", res.img);
     })
     .catch((err) => {
       console.log(err);
     });
+}
+
+document.getElementById("previous").addEventListener("click", getPreviousComic);
+
+async function getPreviousComic() {
+  document.getElementById("next").disabled = false;
+  const newNumber = currentNumber--;
+  const response = await fetch(`https://xkcd.com/${newNumber}/info.0.json`);
+  await response.json().then((data) => {
+    console.log(data);
+    document.getElementById("iframe").setAttribute("src", data.img);
+  });
 }
